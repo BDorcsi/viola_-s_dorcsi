775104129bdb01639b97c9b216550fb6c3ec23af
BDorcsi <71967592+BDorcsi@users.noreply.github.com>
2021-11-11 12:50:27 +0100

    Update 11.04.py

--- 11.04.py
+++ 11.04.py
@@ -1,4 +1,9 @@
 def f1(a,b):
+   print(Képlet: a*x+b=0")
+   a = float(input("Kérem az 'a' értékét: "))
+   b = float(input("Kérem az 'b' értékét: "))
+         if a != 0:
+         x = -b/a
     if  a==0:
         if b==0:
             print("bármelyik szám megoldja az egyenletet")
